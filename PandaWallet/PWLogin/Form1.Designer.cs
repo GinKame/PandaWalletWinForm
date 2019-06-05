5fa8e6d330d6039f44a15928b7e72b722e3097eb
oTenshi <viktor226922@gmail.com>
2019-06-05 15:49:47 +0200

    a
    singleton

---
+++ PandaWallet/PWLogin/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PWLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.reg_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username/e-mail";
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(202, 128);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(75, 23);
+            this.login_btn.TabIndex = 1;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = true;
+            // 
+            // reg_btn
+            // 
+            this.reg_btn.Location = new System.Drawing.Point(202, 174);
+            this.reg_btn.Name = "reg_btn";
+            this.reg_btn.Size = new System.Drawing.Size(75, 23);
+            this.reg_btn.TabIndex = 2;
+            this.reg_btn.Text = "Register";
+            this.reg_btn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 239);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.reg_btn);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Button reg_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ PandaWallet/PWLogin/SingleDB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace PWLogin
+{
+    /// <summary>
+    /// Adatbázis kapcsolat létesítés singletonnal
+    /// </summary>
+    class SingleDB
+    {
+        private static SingleDB uniqueInstance = null;
+        private MySqlConnection connection;
+        private string server;
+        private string port;
+        private string database;
+        private string uid;
+        private string password;
+
+        private SingleDB()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            port = "3306";
+            database = "pandawallet";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" +
+                               "DATABASE=" + database + ";" +
+                               "UID=" + uid + ";" +
+                               "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+            MySqlCommand comm = connection.CreateCommand();
+            
+
+        }
+
+        /// <summary>
+        /// publikus metódus, mellyel meghívjuk a privát konstruktort
+        /// </summary>
+        /// <returns></returns>
+        public static SingleDB getInstance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new SingleDB();
+            return uniqueInstance;
+        }
+
+    }
+}
